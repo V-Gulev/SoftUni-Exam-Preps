9ab90d753cb531873000850e458f3ac696b6366b
V-Gulev <velizargulev@abv.bg>
2025-09-29 19:14:43 +0300

    Mobiles Skeleton Validation

---
+++ Mobiles/src/main/java/softuni/exam/util/ValidationUtilImpl.java
@@ -0,0 +1,24 @@
+package softuni.exam.util;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidationUtilImpl implements ValidationUtil {
+
+    private Validator validator;
+
+    public ValidationUtilImpl() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+
+    }
+
+    @Override
+    public <E> boolean isValid(E entity) {
+        return validator.validate(entity).isEmpty();
+    }
+}
